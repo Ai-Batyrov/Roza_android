8d7b3b6b1745f2568cd5e104f75651a1acce0924
Ai-Batyrov <aibolat.batyrov.03@gmail.com>
2022-10-25 21:07:53 +0600

    исползовал в MainActivity

--- app/src/main/java/kz/sdu/roza/MainActivity.kt
+++ app/src/main/java/kz/sdu/roza/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kz.sdu.roza.data.datasources.FavoriteTracksDataSource
+import kz.sdu.roza.presentation.menu.favorites.FavoriteTrackListAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +17,8 @@ class MainActivity : AppCompatActivity() {
 
         trackList = findViewById(R.id.favorite_recycler_view)
 
-        val layoutManager: LinearLayoutManager = LinearLayoutManager(this)
+        val layoutManager = LinearLayoutManager(this)
+        trackList.adapter = FavoriteTrackListAdapter(FavoriteTracksDataSource().loadTracks())
         trackList.layoutManager = layoutManager
         trackList.setHasFixedSize(true)
     }
