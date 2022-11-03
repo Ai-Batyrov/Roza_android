2861b995f4980ac5d24eb5cc0378daacf9a70118
Ai-Batyrov <aibolat.batyrov.03@gmail.com>
2022-11-03 06:32:46 +0600

    добавил фрагменты л разметками

--- app/src/main/java/kz/sdu/roza/MainActivity.kt
+++ app/src/main/java/kz/sdu/roza/MainActivity.kt
@@ -3,11 +3,10 @@ package kz.sdu.roza
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kz.sdu.roza.data.datasources.FavoriteTracksDataSource
 import kz.sdu.roza.databinding.ActivityMainBinding
-import kz.sdu.roza.presentation.menu.favorites.FavoriteTrackListAdapter
+import kz.sdu.roza.presentation.menu.favorites.FavoritesMainFragment
+import kz.sdu.roza.presentation.menu.home.HomeMainFragment
+import kz.sdu.roza.presentation.menu.search.SearchMainFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +17,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setFragment(HomeMainFragment())
+
+        binding.mainActivityBottomNavigationBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_nav_home_item -> setFragment(HomeMainFragment())
+                R.id.bottom_nav_search_item -> setFragment(SearchMainFragment())
+                R.id.bottom_nav_favorites_item -> setFragment(FavoritesMainFragment())
+
+                else -> {}
+            }
+            true
+        }
     }
 
     private fun setFragment(fragment: Fragment) {
