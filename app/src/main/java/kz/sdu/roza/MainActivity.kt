9c6439a6fb35c2ddb55d2b16764d8029aa757fd4
Ai-Batyrov <aibolat.batyrov.03@gmail.com>
2022-11-03 06:11:33 +0600

    навигация қосылды, Gradle buildFeature -> viewBinding қосылды

--- app/src/main/java/kz/sdu/roza/MainActivity.kt
+++ app/src/main/java/kz/sdu/roza/MainActivity.kt
@@ -5,21 +5,17 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kz.sdu.roza.data.datasources.FavoriteTracksDataSource
+import kz.sdu.roza.databinding.ActivityMainBinding
 import kz.sdu.roza.presentation.menu.favorites.FavoriteTrackListAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trackList: RecyclerView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        trackList = findViewById(R.id.favorite_recycler_view)
-
-        val layoutManager = LinearLayoutManager(this)
-        trackList.adapter = FavoriteTrackListAdapter(FavoriteTracksDataSource().loadTracks())
-        trackList.layoutManager = layoutManager
-        trackList.setHasFixedSize(true)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
