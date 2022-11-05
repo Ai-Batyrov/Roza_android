28656b6998bd7656cb475f6ab30a31bf97b71ce6
inspiredsamat <zhumamuratovsamat@gmail.com>
2022-11-05 16:15:04 +0600

    implemented methods of PlaylistFragment.kt

--- app/src/main/java/kz/sdu/roza/presentation/menu/playlists/PlaylistFragment.kt
+++ app/src/main/java/kz/sdu/roza/presentation/menu/playlists/PlaylistFragment.kt
@@ -5,28 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kz.sdu.roza.R
+import kz.sdu.roza.data.datasources.TurkishTracksDataSource
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val TRACK_COUNT = "0"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PlaylistFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PlaylistFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var trackList: RecyclerView
+    private var trackCount: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            trackCount = it.getString(TRACK_COUNT)
         }
     }
 
@@ -34,26 +29,24 @@ class PlaylistFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false)
+        val view = inflater.inflate(R.layout.fragment_playlist, container, false)
+        trackList = view.findViewById(R.id.playlist_recycler_view)
+        trackList.layoutManager = LinearLayoutManager(activity)
+        trackList.adapter = PlaylistAdapter(TurkishTracksDataSource().loadTracks())
+        val sortOptions = resources.getStringArray(R.array.sort_options)
+        val arrayAdapter = ArrayAdapter(view.context, R.layout.dropdown_item, sortOptions)
+        val autoCompleteTV = view.findViewById<AutoCompleteTextView>(R.id.sortByTextView)
+        autoCompleteTV.setAdapter(arrayAdapter)
+        trackList.setHasFixedSize(true)
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlaylistFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             PlaylistFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(TRACK_COUNT, trackCount)
                 }
             }
     }
