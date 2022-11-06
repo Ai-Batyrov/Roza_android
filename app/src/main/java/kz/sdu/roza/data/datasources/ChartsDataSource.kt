b15078f7f18dac771a0287e7efd30293b969f3f1
Ai-Batyrov <aibolat.batyrov.03@gmail.com>
2022-11-07 03:20:57 +0600

    Создал абстракный ListsDataSource, чтобы в будущем использовать для остальной части

--- app/src/main/java/kz/sdu/roza/data/datasources/ChartsDataSource.kt
+++ app/src/main/java/kz/sdu/roza/data/datasources/ChartsDataSource.kt
@@ -1,4 +1,15 @@
 package kz.sdu.roza.data.datasources
 
-class ChartsDataSource {
+import kz.sdu.roza.data.models.Chart
+
+class ChartsDataSource : ListsDataSource() {
+    private lateinit var charts: List<Chart>
+
+    override fun setList(data: List<Any>) {
+        charts = data as List<Chart>
+    }
+
+    override fun getList(): List<Any> {
+        return charts
+    }
 }
